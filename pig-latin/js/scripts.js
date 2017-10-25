ee8a62889b40ed564b52777c2625b1a2a54adccc
Adam Smith and Mikey Pinaud <adam@>
2017-10-25 13:02:35 -0700

    add multiple functions to split up words and add way to the end of words beginning with a vowel

--- pig-latin/js/scripts.js
+++ pig-latin/js/scripts.js
@@ -1,8 +1,33 @@
+var alphaChar = /[A-Za-z]/;
+var vowels = /[aeiouAEIOU]/;
+var pigLatinWords = [];
+var pigLatinSentence = [];
+
+var words = function(sentence) {
+  return sentence.split(" ");
+};
+
+var letters = function(word) {
+  return word.split("");
+};
+
+var iglatinPay = function(userSentence) {
+  var userWords = words(userSentence);
+  userWords.forEach(function(word) {
+    var userLetters = letters(word);
+    if (vowels.test(userLetters[0])) {
+      userLetters.push("w", "a", "y");
+      console.log(userLetters);
+    };
+
+  });
+};
+
 $(function(){
   $("#pig-latin").submit(function(event){
     event.preventDefault();
-    var input = $("#user-input").val();
-    var pigLatin = translate(input);
+    var userInput = $("#user-input").val();
+    var pigLatin = iglatinPay(userInput);
     $("#result").text(pigLatin);
   });
 });
